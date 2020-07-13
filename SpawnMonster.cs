89a785848aaa22f000b69e89dec451572f719d18
yarawtf <66222978+yarawtf@users.noreply.github.com>
2020-07-13 15:07:04 +0300

    Добавлены задачи 1,2,3

---
+++ SpawnMonster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMonster : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject monster;
+    public float secondsBetweenSpawn = 5;
+    public float elapsedTime = 0.0f; 
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetweenSpawn)
+        {
+            elapsedTime = 0;
+            Instantiate(monster, transform);
+        }
+    }
+}
